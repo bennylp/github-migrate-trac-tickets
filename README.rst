74d7b2f64c277136a1e3f2b3e0442844bc421e1e
Dave Hershberger <hersh@willowgarage.com>
2012-10-01 15:36:49 -0700

    added command line option to migrate milestones (disabled by default), added command line argument for original trac URL, renamed script to migrate.py, and renamed README.txt to README.rst and reformatted readme to restructured text.

---
+++ README.rst
@@ -0,0 +1,53 @@
+Migrate Trac Tickets to GitHub Issues
+=====================================
+
+The "migrate.py" script copies Trac tickets into GitHub Issues.
+
+It will optionaly create and merge "Milestones".
+
+It selects only trac tickets with a given "Component" for migration.
+
+It makes GitHub "Labels" for both trac "Priority" and trac "type"
+(where type is "defect", "enhancement", or "task").  It translates
+"defect" to "bug", which exists by default on GitHub.
+
+It cannot migrate ticket ownership to GitHub Issue "Assignee" since we
+have no way to map customer-specific Trac usernames into global GitHub
+usernames.
+
+The new github issue numbers do not match the old Trac ticket numbers,
+though the numbers are saved in the description for reference.
+
+Extra metadata added to GitHub issues
+-------------------------------------
+
+The script adds the following info to the description of each issue:
+
+ * original owner
+ * original reporter
+ * original date reported
+ * URL of the original trac ticket
+
+The base of the trac ticket URL must be given on the command line.
+
+Testing out migration
+---------------------
+
+The GitHub API has no way to DELETE an Issue (you can only close
+them), so **it is important to test this first**.
+
+Create a temporary new GitHub repository, like 'yourorg/killme'. Then
+push your Trac tickets into it, for example::
+
+  ./migrate.py ~/oldproject-trac.db http://example.com/trac/oldproject yourtraccomponent yourname yourpasswd yourorg/killme
+
+Then verify labels, metadata, back-links and optionally milestones
+migrated as expected. Finally, destroy the test repository.
+
+If you make a mistake and migrate a bunch of tickets incorrectly to a
+repo which is **not** a test repo, you can still recover to some extent.
+(This will lose any issues created within github that aren't in your
+trac.)  Ensure you've pulled a current copy of the code repo, then
+blow away the github repo and re-create it.  Migrate the tickets
+(correctly this time), then finally push your local code repo back
+into the code repo.
--- migrate.py
+++ migrate.py
@@ -6,6 +6,8 @@
 # get multiples if you run repeatedly.  See API docs:
 # http://developer.github.com/v3/issues/
 
+import os
+import sys
 import datetime
 import logging
 from optparse import OptionParser
@@ -36,22 +38,33 @@ class Trac(object):
 
 # Warning: optparse is deprecated in python-2.7 in favor of argparse
 usage = """
-  %prog [options] trac_db_path trac_component github_username github_password github_repo
+  %prog [options] trac_db_path trac_url trac_component github_username github_password github_repo
 
-  The path might be something like "/tmp/trac.db"
-  The github_repo combines user or organization and specific repo like "myorg/myapp"
+  trac_db_path might be something like "/tmp/trac.db".
+    It is the sqlite database file used by the trac instance, or a copy thereof.
+  trac_url should point to the root of the trac project, like http://code.ros.org/trac/ros-pkg for ros-pkg.
+  trac_component is the component whose tickets you want to migrate.
+  github_repo combines user or organization and repository like "myorg/myapp"
 """
 parser = OptionParser(usage=usage)
 parser.add_option('-q', '--quiet', action="store_true", default=False,
                   help='Decrease logging of activity')
+parser.add_option('-m', '--milestones', action="store_true", default=False,
+                  help='Migrate trac milestones to github milestones')
 
 (options, args) = parser.parse_args()
 try:
-    [trac_db_path, trac_component, github_username, github_password, github_repo] = args
+    [trac_db_path, trac_url, trac_component, github_username, github_password, github_repo] = args
 except ValueError:
-    parser.error('Wrong number of arguments')
+    print('Wrong number of arguments')
+    parser.print_help()
+    sys.exit(1)
 if not '/' in github_repo:
-    parser.error('Repo must be specified like "organization/project"')
+    print('Invalid repo "%s".  Repo must be specified like "organization/project"' % github_repo)
+    sys.exit(1)
+if not os.path.exists(trac_db_path):
+    print('trac sqlite database file "%s" not found.' % trac_db_path)
+    sys.exit(1)
 
 if options.quiet:
     logging.basicConfig(level=logging.INFO)
@@ -69,7 +82,6 @@ for (username,) in trac.sql('SELECT DISTINCT owner FROM ticket WHERE component="
         username = username.strip() # username returned is tuple like: ('phred',)
         logging.debug("Trac ticket owner: %s" % username)
 
-
 # Get GitHub labels; we'll merge Trac priorities and types into them
 
 logging.info("Getting existing GitHub labels...")
@@ -78,44 +90,45 @@ for label in github.labels():
     gh_labels[label['name']] = True
     logging.debug("label name=%s" % label['name'])
 
-# Get any existing GitHub milestones so we can merge Trac into them.
-# We need to reference them by numeric ID in tickets.
-# API returns only 'open' issues by default, have to ask for closed like:
-# curl -u 'USER:PASS' https://api.github.com/repos/USERNAME/REPONAME/milestones?state=closed
+if options.milestones:
+    # Get any existing GitHub milestones so we can merge Trac into them.
+    # We need to reference them by numeric ID in tickets.
+    # API returns only 'open' issues by default, have to ask for closed like:
+    # curl -u 'USER:PASS' https://api.github.com/repos/USERNAME/REPONAME/milestones?state=closed
 
-logging.info("Getting existing GitHub milestones...")
-milestone_id = {}
-for m in github.milestones():
-    milestone_id[m['title']] = m['number']
-    logging.debug("milestone (open)   title=%s" % m['title'])
-for m in github.milestones(query='state=closed'):
-    milestone_id[m['title']] = m['number']
-    logging.debug("milestone (closed) title=%s" % m['title'])
+    logging.info("Getting existing GitHub milestones...")
+    milestone_id = {}
+    for m in github.milestones():
+        milestone_id[m['title']] = m['number']
+        logging.debug("milestone (open)   title=%s" % m['title'])
+    for m in github.milestones(query='state=closed'):
+        milestone_id[m['title']] = m['number']
+        logging.debug("milestone (closed) title=%s" % m['title'])
 
-# We have no way to set the milestone closed date in GH.
-# The 'due' and 'completed' are long ints representing datetimes.
+    # We have no way to set the milestone closed date in GH.
+    # The 'due' and 'completed' are long ints representing datetimes.
 
-logging.info("Migrating Trac milestones to GitHub...")
-milestones = trac.sql('SELECT name, description, due, completed FROM milestone')
-for name, description, due, completed in milestones:
-    name = name.strip()
-    logging.debug("milestone name=%s due=%s completed=%s" % (name, due, completed))
-    if name and name not in milestone_id:
-        if completed:
-            state = 'closed'
-        else:
-            state = 'open'
-        milestone = {'title': name,
-                     'state': state,
-                     'description': description,
-                     }
-        if due:
-            milestone['due_on'] = datetime.datetime.fromtimestamp( due ).isoformat()
-        logging.debug("milestone: %s" % milestone)
-        gh_milestone = github.milestones(data=milestone)
-        milestone_id['name'] = gh_milestone['number']
+    logging.info("Migrating Trac milestones to GitHub...")
+    milestones = trac.sql('SELECT name, description, due, completed FROM milestone')
+    for name, description, due, completed in milestones:
+        name = name.strip()
+        logging.debug("milestone name=%s due=%s completed=%s" % (name, due, completed))
+        if name and name not in milestone_id:
+            if completed:
+                state = 'closed'
+            else:
+                state = 'open'
+            milestone = {'title': name,
+                         'state': state,
+                         'description': description,
+                         }
+            if due:
+                milestone['due_on'] = datetime.datetime.fromtimestamp( due ).isoformat()
+            logging.debug("milestone: %s" % milestone)
+            gh_milestone = github.milestones(data=milestone)
+            milestone_id['name'] = gh_milestone['number']
 
-# Copy Trac tickets to GitHub issues, keyed to milestones above
+# Copy Trac tickets to GitHub issues, optionally keyed to milestones above
 
 tickets = trac.sql('SELECT id, priority, type, summary, description, owner, reporter, milestone, time, status FROM ticket WHERE component="%s" ORDER BY id' % trac_component) # LIMIT 5
 for tid, priority, ticket_type, summary, description, owner, reporter, milestone, timestamp, status in tickets:
@@ -150,9 +163,9 @@ for tid, priority, ticket_type, summary, description, owner, reporter, milestone
         description += " * Owner: **%s**\n" % owner
         description += " * Reporter: **%s**\n" % reporter
         description += " * Reported at: **%s**\n" % datetime.datetime.fromtimestamp( timestamp ).ctime()
-        description += " * URL: http://code.ros.org/trac/ros-pkg/ticket/%d" % tid
+        description += " * URL: %s/ticket/%d" % (trac_url, tid)
         issue['body'] = description
-    if milestone:
+    if milestone and options.milestones:
         m = milestone_id.get(milestone)
         if m:
             issue['milestone'] = m
@@ -179,5 +192,3 @@ for tid, priority, ticket_type, summary, description, owner, reporter, milestone
         logging.debug("close")
 
 trac.close()
-
-
